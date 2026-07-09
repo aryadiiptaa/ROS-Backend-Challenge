93d69a8c3d21b5ad92bfc9fdc15962376addece3
aryadiiptaa <ihsanaryafiisp@gmail.com>
2026-07-09 16:14:48 +0700

    Initial commit: Setup arena and rover robot with Gazebo Harmonic

---
+++ launch/spawn.launch.py
@@ -0,0 +1,48 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_name = 'robot_ros_backend'
+
+    urdf_file = os.path.join(get_package_share_directory(pkg_name), 'urdf', 'robot.urdf')
+    world_file = os.path.join(get_package_share_directory(pkg_name), 'worlds', 'arena.sdf')
+
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': f'-r -v 4 {world_file}'}.items()
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'challenge_rover',
+            '-topic', 'robot_description',
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.5'
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        gazebo,
+        spawn_robot
+    ])
